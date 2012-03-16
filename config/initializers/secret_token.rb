8fe7eed0724afb35f48c6d8558ace9641c7f49ff
Daniel Fujimoto Gamero <daniel.fujimotog@gmail.com>
2012-03-15 20:53:55 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TdewPc1::Application.config.secret_token = '27a15a9d1259f8987e6dd6f68ba0a34f8aa7ed35ea6bacfd2c90e259ab0731c65211490ca7cd2e6b11ab4334df326a2fe22b65a7a862d42aa0e54d47591bb038'
